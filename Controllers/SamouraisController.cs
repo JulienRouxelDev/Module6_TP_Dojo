8e5b9dff1ea854311d550c0774d6e0995b13917d
JulienRouxel <rouxel.julien@orange.fr>
2020-03-23 15:10:02 +0100

    Initial commit

---
+++ Controllers/SamouraisController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BO;
+using Dojo.Data;
+using Dojo.Models;
+
+namespace Dojo.Controllers
+{
+    public class SamouraisController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Samourais
+        public ActionResult Index()
+        {
+            return View(db.Samourais.ToList());
+        }
+
+        // GET: Samourais/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Samourai samourai = db.Samourais.Find(id);
+            if (samourai == null)
+            {
+                return HttpNotFound();
+            }
+            return View(samourai);
+        }
+
+        // GET: Samourais/Create
+        public ActionResult Create()
+        {
+            SamouraiVM samouraiVM = new SamouraiVM();
+            return View(samouraiVM);
+        }
+
+        // POST: Samourais/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SamouraiVM samouraiVM)
+        {
+            if (ModelState.IsValid)
+            {
+                Samourai samourai = new Samourai();
+                Arme arme = new Arme();
+
+                arme = db.Armes.Find(samouraiVM.IdArme);
+
+                samourai.Force = samouraiVM.Samourai.Force;
+                samourai.Nom = samouraiVM.Samourai.Nom;
+                samourai.Arme = arme;
+
+                db.Samourais.Add(samourai);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(samouraiVM);
+        }
+
+        // GET: Samourais/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Samourai samourai = db.Samourais.Find(id);
+            
+            if (samourai == null)
+            {
+                
+                return HttpNotFound();
+            }
+
+            SamouraiVM samouraiVM = new SamouraiVM();
+            samouraiVM.Samourai = samourai;
+
+            return View(samouraiVM);
+        }
+
+        // POST: Samourais/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(SamouraiVM samouraiVM)
+        {
+            if (ModelState.IsValid)
+            {
+                Samourai samouraiBd = db.Samourais.Find(samouraiVM.Samourai.Id);
+                Arme arme = db.Armes.Find(samouraiVM.IdArme);
+
+                samouraiBd.Force = samouraiVM.Samourai.Force;
+                samouraiBd.Nom = samouraiVM.Samourai.Nom;
+                samouraiBd.Arme = arme;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(samouraiVM);
+        }
+
+        // GET: Samourais/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Samourai samourai = db.Samourais.Find(id);
+            if (samourai == null)
+            {
+                return HttpNotFound();
+            }
+            return View(samourai);
+        }
+
+        // POST: Samourais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Samourai samourai = db.Samourais.Find(id);
+            db.Samourais.Remove(samourai);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/SamouraiVM.cs
@@ -0,0 +1,43 @@
+﻿using BO;
+using Dojo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dojo.Models
+{
+    public class SamouraiVM
+    {
+		private Samourai samourai;
+		private List<Arme> armesDisponibles;
+		private int idArme;
+
+		public int IdArme
+		{
+			get { return idArme; }
+			set { idArme = value; }
+		}
+
+
+		public List<Arme> ArmesDisponibles
+		{
+			get { return this.armesDisponibles; }
+			set { this.armesDisponibles = value; }
+		}
+
+
+		public Samourai Samourai
+		{
+			get { return this.samourai; }
+			set { this.samourai = value; }
+		}
+
+		public SamouraiVM()
+		{
+			Context db = new Context();
+			this.armesDisponibles = db.Armes.ToList();
+		}
+
+	}
+}
